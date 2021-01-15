a3375a26d25363c2d15047dd295d8d6a7d634667
LiTuo <lituo7516@gmail.com>
2021-01-15 10:07:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,13 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# add N1 openwrt to EMMC
+# make menuconfig
+# Utilities  --->  
+#   <*> install-program
+git clone https://github.com/tuanqing/install-program package/install-program
